fc3127aceac4065b072de06031afda1f1acad20d
willhorning <will.horning@gmail.com>
2014-05-03 10:17:40 -0400

    github first commit

--- nbaapi/__init__.py
+++ nbaapi/__init__.py
@@ -9,19 +9,4 @@ app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['DEBUG'] = True
 db = SQLAlchemy(app)
 
-from nbaapi.models import Shot, Player
-ps = db.session.query(Player).all()
-d = {}
-for p in ps:
-    ss = []
-    for s in p.shots:
-        fields = s.__dict__.copy()
-        del fields['_sa_instance_state']
-        del fields['datetime']
-        ss.append(fields)
-    d[p.full_name] = ss
-f = open('foo.json', 'w')
-json.dump(d, f)
-f.close()
-
 import nbaapi.views
