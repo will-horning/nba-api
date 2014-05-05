477cda4184fb121b3933b7307189144673fd75e8
willhorning <will.horning@gmail.com>
2014-05-05 09:01:09 -0400

    removed old setting file

--- settings.py
+++
@@ -1,6 +0,0 @@
-DATABASE = {'drivername': 'postgres',
-            'host': 'localhost',
-            'port': '5432',
-            'username': 'root',
-            'password': 'root',
-            'database': 'nba_api'}
